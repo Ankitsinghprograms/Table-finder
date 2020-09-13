d0ff38ea0b62fa02710453d79239b1d11ce5ee24
Ankit Singh <67016832+Ankitsinghprograms@users.noreply.github.com>
2020-09-13 12:09:47 +0530

    Add files via upload

---
+++ table finder.py
@@ -0,0 +1,10 @@
+print("Table finder\n")
+print("Enter the number of which you want table\n")
+no=int(input())
+print("\nSo You want table of",no)
+print("\nHere is the table of",no,"\n")
+times=1
+total=no*10
+while 10>=times:
+	print(no,"times",times,"=",no*times)
+	times=times+1
